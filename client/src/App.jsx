8736803f3949d0550ba2cc09010b96bf8d686f4e
PedroBettencourt <pedropbettencourt@gmail.com>
2025-09-30 12:01:29 +0100

    Add api and add task fetching

---
+++ client/src/App.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+
+function Tasks() {
+
+  const [tasks, setTasks] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getTasks() {
+      setIsLoading(true);
+      setError(false);
+      try {
+        const res = await fetch(import.meta.env.VITE_URL);
+        const json = await res.json();
+        setTasks(json);
+      } catch(err) {
+        setError(err);
+        console.error(err)
+      } finally {
+        setIsLoading(false);
+      };    
+    };
+
+    getTasks();
+  }, []);
+
+  
+  return (
+    <>
+      { isLoading && <h3>Loading...</h3> }
+      { error && <h3>Error</h3> }
+      
+      { tasks &&
+        <ul>
+          { tasks.map(task => ( 
+            <li key={ task.id }>{ task.text }</li>
+          ))}
+        </ul>
+      }
+    </>
+  )
+};
+
+function App() {
+
+
+  return(
+    <>
+      <header>
+        <h1>New App</h1>
+      </header>
+
+      <div className="main">
+        <nav>
+          <h2>Home</h2>
+          <h2>Tasks</h2>
+        </nav>
+        
+        <section>
+          <h2>Task List</h2>
+          <Tasks />
+        </section>
+      </div>
+    
+      <footer>
+        
+      </footer>
+    </>
+    
+  )
+
+};
+
+export default App;
